6d32289540f9f491b43a98fc0c54b694cd7fc7c7
Stefan123 <cizmicstefan731@gmail.comm>
2024-04-30 11:43:25 +0200

    Location.js upgraded

--- my-app/src/Components/Location/Location.js
+++ my-app/src/Components/Location/Location.js
@@ -33,8 +33,6 @@ const Location = ({ currentLocation }) => {
         <div className="errorMessage">
           <h2>Error</h2>
           <p>Failed to load map. Please allow access to your location</p>
-          <p>a</p>
-          <p>a</p>
         </div>
       )}
     </SidebarLayout>
