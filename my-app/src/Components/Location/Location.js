3ccbe86c29debf1b3137eeafab72dc927df2b08e
Stefan123 <cizmicstefan731@gmail.comm>
2024-05-01 12:08:43 +0200

    Latest version

--- my-app/src/Components/Location/Location.js
+++ my-app/src/Components/Location/Location.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import SidebarLayout from "../SidebarLayout/SidebarLayout";
 import "./Location.css";
 import "leaflet/dist/leaflet.css";
