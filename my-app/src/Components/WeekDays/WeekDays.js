6cec77e0c84662c7743a5b33a777c304ebdb45ce
Stefan123 <cizmicstefan731@gmail.comm>
2024-11-18 14:42:18 +0100

    finished

--- my-app/src/Components/WeekDays/WeekDays.js
+++ my-app/src/Components/WeekDays/WeekDays.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeekDays.css";
 import { useScreenWidth } from "../../Hooks/useScreenWidth";
 
 const WeekDays = ({ weekly, setShowWeekly }) => {
+  const [selectedDay, setSelectedDay] = useState(0);
   const screenWidth = useScreenWidth();
 
   return (
     <div className="week-days">
       {weekly.map((day, index) => (
-        <div key={index} className="days" onClick={() => setShowWeekly(day)}>
-          <small>Day</small>
+        <div
+          key={index}
+          className="days"
+          onClick={() => {
+            setSelectedDay(index);
+            setShowWeekly(day);
+          }}
+          style={{
+            background:
+              selectedDay === index
+                ? "linear-gradient(to right, #B76BCC, #7FBFFF, #00B8B8)"
+                : "",
+            cursor: "pointer",
+          }}
+        >
+          <small className="day-small-p">Day</small>
           <p className="day">{day.date}</p>
           {screenWidth <= 1150 && (
             <div className="responsive-temps">
